95232632dae8654b18ae2366d277b6641d486200
anfal11 <anfal15-14303@diu.edu.bd>
2023-10-19 00:11:43 +0600

    addProduct option implemented with backend and data is sendting to db

--- src/Pages/AddProduct.jsx
+++ src/Pages/AddProduct.jsx
@@ -1,5 +1,42 @@
+import Swal from "sweetalert2";
 
 const AddProduct = () => {
+
+    const handleAdd = (e) => {
+        e.preventDefault();
+        const form = new FormData(e.target);
+        const name = form.get("name");
+        const type = form.get("type");
+        const brand = form.get("brand");
+        const price = form.get("price");
+        const description = form.get("description");
+        const rating = form.get("rating");
+        const image = form.get("image");
+
+        const data = {name, type, brand, price, description, rating, image};
+
+        e.target.reset();
+
+        fetch("http://localhost:5000/addProduct", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.insertedId){
+                Swal.fire(
+                    'Good job!',
+                    'Product Added Successfully',
+                    'success'
+                  )
+            }
+        })
+    }
+
   return (
 
     <div className="max-w-[1320px] mx-auto">
@@ -15,7 +52,7 @@ const AddProduct = () => {
             </p>
     
             {/* form */}
-            <form>
+            <form onSubmit={handleAdd}>
             <div className="flex flex-col items-center lg:items-stretch">
     
     
@@ -36,19 +73,24 @@ const AddProduct = () => {
               </div>
     
              {/* type */}
-              <div className="form-control lg:pr-36">
-                <label className="label">
-                  <span className="label-text font-Raleway font-semibold text-xl">Image Link</span>
-                </label>
-                <label className="input-group">
-                  <input
-                  name="image"
-                    type="text"
-                    placeholder="Enter image link"
-                    className="md:w-[300px] xl:w-[500px] p-3 input-bordered"
-                  />
-                </label>
-              </div>
+             <div className="form-control lg:pr-36">
+  <label className="label">
+    <span className="label-text font-Raleway font-semibold text-xl">Type</span>
+  </label>
+  <label className="input-group">
+    <select
+      name="type"
+      className="md:w-[300px] xl:w-[500px] p-3 input-bordered"
+    >
+      <option value="">Select a type</option>
+      <option value="phone">Phone</option>
+      <option value="computer">Watch</option>
+      <option value="headphone">Headphone</option>
+      <option value="headphone">Laptop</option>
+    </select>
+  </label>
+</div>
+
     </div>
     
     <div className="flex flex-col lg:flex-row justify-between">
@@ -93,7 +135,7 @@ const AddProduct = () => {
                 </label>
                 <label className="input-group">
                   <input
-                  name="dexcription"
+                  name="description"
                     type="text"
                     placeholder="Type a short description"
                     className="md:w-[300px] xl:w-[500px] p-3 input-bordered"
